186eb18b8509156e51e642ce4ef994299478f9bc
spacemeowx2 <spacemeowx2@gmail.com>
2022-12-14 19:03:12 +0800

    refactor: move User-Agent

--- src/iksm.ts
+++ src/iksm.ts
@@ -42,8 +42,7 @@ export async function loginManually(
         "Connection": "keep-alive",
         "Cache-Control": "max-age=0",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent":
-          "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36",
+        "User-Agent": DEFAULT_APP_USER_AGENT,
         "Accept":
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8n",
         "DNT": "1",
