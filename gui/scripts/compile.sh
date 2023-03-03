2474794b93c0246ac5d002e43562e5d982ea5410
spacemeowx2 <spacemeowx2@gmail.com>
2023-03-03 22:13:09 +0800

    build: add -A to `deno compile`

--- gui/scripts/compile.sh
+++ gui/scripts/compile.sh
@@ -3,5 +3,5 @@ TARGETS="x86_64-unknown-linux-gnu,x86_64-pc-windows-msvc,x86_64-apple-darwin,aar
 # compile for every target in bash
 for target in $(echo $TARGETS | sed "s/,/ /g")
 do
-    deno compile --target=$target -o ./binaries/s3si-$target ../s3si.ts
+    deno compile --target=$target -o ./binaries/s3si-$target -A ../s3si.ts
 done
