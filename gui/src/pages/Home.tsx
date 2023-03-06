d0ee4de9163c4de2a161291b2c2e85553173e5b3
imspace <spacemeowx2@gmail.com>
2023-03-07 04:04:00 +0800

    feat: add sesionToken field in settings

--- gui/src/pages/Home.tsx
+++ gui/src/pages/Home.tsx
@@ -1,9 +1,30 @@
+import { ErrorContent } from 'components/ErrorContent';
+import { Loading } from 'components/Loading';
+import { usePromise } from 'hooks/usePromise';
 import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { Link } from "react-router-dom";
+import { getConfig, getProfile } from 'services/config';
+import { composeLoadable } from 'utils/composeLoadable';
 
 export const Home: React.FC = () => {
+  let { loading, error, retry, result } = composeLoadable({
+    config: usePromise(getConfig),
+    profile: usePromise(() => getProfile(0)),
+  });
   const { t } = useTranslation();
+
+  if (loading) {
+    return <>
+      <div className='h-full flex items-center justify-center'><Loading /></div>
+    </>
+  }
+
+  if (error) {
+    return <>
+      <ErrorContent error={error} retry={retry} />
+    </>
+  }
 
   return <>
     <div className='card m-2 h-full'>
