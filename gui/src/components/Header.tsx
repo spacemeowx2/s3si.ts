7532fde754f55a1ee859047346896a0cdd64697b
imspace <spacemeowx2@gmail.com>
2023-03-08 22:55:23 +0800

    refactor: extract components

---
+++ gui/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AiOutlineLeft } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+
+type HeaderProps = {
+  title?: React.ReactNode
+}
+
+export const Header: React.FC<HeaderProps> = ({ title }) => {
+  const navigate = useNavigate();
+  return <>
+    <h2 className="card-title" data-tauri-drag-region><button onClick={() => navigate('/')}><AiOutlineLeft /></button>{title}</h2>
+  </>
+}
