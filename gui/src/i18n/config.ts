1544fd4989c28cee083de4a35040677d85a753e9
imspace <spacemeowx2@gmail.com>
2023-03-08 22:54:46 +0800

    feat: add i18next http backend

--- gui/src/i18n/config.ts
+++ gui/src/i18n/config.ts
@@ -2,6 +2,7 @@ import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import translation from './translation/en.json';
+import HttpBackend from 'i18next-http-backend'
 
 export const resources = {
   en: {
@@ -9,10 +10,17 @@ export const resources = {
   }
 };
 
-i18next
+const instance = i18next
   .use(initReactI18next)
-  .use(LanguageDetector)
-  .init({
-    debug: import.meta.env.DEV,
-    resources,
-  });
+  .use(LanguageDetector);
+
+if (import.meta.env.DEV) {
+  instance.use(HttpBackend);
+}
+
+instance.init({
+  debug: import.meta.env.DEV,
+  resources,
+  saveMissing: true,
+  fallbackLng: 'en'
+});
