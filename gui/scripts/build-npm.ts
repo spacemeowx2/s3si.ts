a0edf4daa2f3306712df08ffe7bbbb29e7d9c53a
spacemeowx2 <spacemeowx2@gmail.com>
2023-03-13 18:56:24 +0800

    refactor: move build-npm

--- gui/scripts/build-npm.ts
+++ gui/scripts/build-npm.ts
@@ -1,10 +1,14 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
+import * as path from "https://deno.land/std@0.178.0/path/mod.ts";
 
-await emptyDir("./npm");
+const __dirname = path.resolve(path.dirname(path.fromFileUrl(import.meta.url)), '..');
+const OUT_DIR = path.join(__dirname, "./packages/s3si-ts");
+
+await emptyDir(OUT_DIR);
 
 await build({
-  entryPoints: ["./src/app.ts"],
-  outDir: "./npm",
+  entryPoints: [path.join(__dirname, "../src/app.ts")],
+  outDir: OUT_DIR,
   shims: {
     // see JS docs for overview and more options
     deno: true,
