c2f42863b88a97b77e28433b534036e7f3457f4b
spacemeowx2 <spacemeowx2@gmail.com>
2023-05-31 21:16:46 +0800

    feat: add Queries fetch in update-constant

--- scripts/update-constant.ts
+++ scripts/update-constant.ts
@@ -30,6 +30,62 @@ function getConst(content: string, name: string): string {
   return JSON.parse(match[1]);
 }
 
+function replaceEnum(
+  content: string,
+  name: string,
+  pairs: Record<string, string>,
+): string {
+  const regex = new RegExp(`export enum ${name} {([\\s\\S^}]+?)}`);
+
+  const body = Object.entries(pairs).map(([key, value]) =>
+    `  ${key} = "${value}"`
+  ).join(",\n");
+
+  return content.replace(regex, `export enum ${name} {\n${body}\n}`);
+}
+
+function getEnumKeys(content: string, name: string): string[] {
+  const regex = new RegExp(`export enum ${name} {([\\s\\S^}]+?)}`);
+
+  const match = regex.exec(content);
+
+  if (!match) {
+    throw new Error(`Cannot find ${name}`);
+  }
+
+  const body = match[1];
+
+  // extract keys from `key = "value"`
+  const keys: string[] = [];
+  const keyRE = /\s*(\w+)\s*=/g;
+  while (true) {
+    const match = keyRE.exec(body);
+    if (!match) {
+      break;
+    }
+    keys.push(match[1]);
+  }
+
+  return keys;
+}
+
+function getQueryHash(js: string, query: string): string {
+  const regex = new RegExp(
+    `params:\\{id:"([^"]*?)",metadata:{},name:"${query}"`,
+  );
+
+  const match = regex.exec(js);
+
+  if (!match) {
+    throw new Error(`Cannot find ${query}`);
+  }
+  if (match[0].length > 500) {
+    throw new Error(`Match too large ${match[0].length}`);
+  }
+
+  return match[1];
+}
+
 async function printError<T>(p: Promise<T>): Promise<T | undefined> {
   try {
     return await p;
@@ -39,7 +95,7 @@ async function printError<T>(p: Promise<T>): Promise<T | undefined> {
   }
 }
 
-async function getWebViewVer(): Promise<string> {
+async function getMainJSBody(): Promise<string> {
   const splatnet3Home = await (await fetch(SPLATNET3_URL)).text();
 
   const mainJS = /src="(\/.*?\.js)"/.exec(splatnet3Home)?.[1];
@@ -50,9 +106,16 @@ async function getWebViewVer(): Promise<string> {
 
   const mainJSBody = await (await fetch(SPLATNET3_URL + mainJS)).text();
 
-  const revision = /"([0-9a-f]{40})"/.exec(mainJSBody)?.[1];
+  return mainJSBody;
+}
+
+const mainJSBody = await getMainJSBody();
+
+// deno-lint-ignore require-await
+async function getWebViewVer(js: string): Promise<string> {
+  const revision = /"([0-9a-f]{40})"/.exec(js)?.[1];
   const version = /revision_info_not_set.*?=("|`)(\d+\.\d+\.\d+)-/.exec(
-    mainJSBody,
+    js,
   )
     ?.[2];
 
@@ -83,7 +146,7 @@ const oldValues = {
 };
 const newValues: Record<string, string | undefined> = {};
 
-newValues.WEB_VIEW_VERSION = await printError(getWebViewVer());
+newValues.WEB_VIEW_VERSION = await printError(getWebViewVer(mainJSBody));
 newValues.NSOAPP_VERSION = await printError(getNSOVer());
 
 for (const [key, value] of Object.entries(newValues)) {
@@ -91,8 +154,27 @@ for (const [key, value] of Object.entries(newValues)) {
     content = replaceConst(content, key, value);
   }
 }
-await Deno.writeTextFile(CONSTANT_PATH, content);
 
-console.log("Done");
+console.log("const updated");
 console.log("Old:", oldValues);
 console.log("New:", newValues);
+
+const keys = getEnumKeys(content, "Queries");
+const pairs = Object.fromEntries(
+  keys.map((key) => [key, getQueryHash(mainJSBody, key)]),
+);
+content = replaceEnum(content, "Queries", pairs);
+console.log("query updated");
+
+await Deno.writeTextFile(CONSTANT_PATH, content);
+
+const command = new Deno.Command(Deno.execPath(), {
+  args: ["fmt", "./src/constant.ts"],
+  cwd: ROOT_DIR,
+  stdin: "inherit",
+  stdout: "inherit",
+});
+const { code } = command.outputSync();
+if (code !== 0) {
+  Deno.exit(code);
+}
