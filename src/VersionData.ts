f500d92a55e73cb17fbe00204f69cdf51fda4654
spacemeowx2 <spacemeowx2@gmail.com>
2023-08-31 17:02:29 +0800

    feat: update VersionData

--- src/VersionData.ts
+++ src/VersionData.ts
@@ -30,6 +30,12 @@ export const SEASONS: Season[] = [
     start: new Date("2023-06-01T00:00:00+00:00"),
     end: new Date("2023-09-01T00:00:00+00:00"),
   },
+  {
+    id: "season202309",
+    name: "Drizzle Season 2023",
+    start: new Date("2023-09-01T00:00:00+00:00"),
+    end: new Date("2023-12-01T00:00:00+00:00"),
+  },
 ];
 
 export const getSeason = (date: Date): Season | undefined => {
