f236a523f7bb51efb7a2ab2390490a7d056f0a4e
spacemeowx2 <spacemeowx2@gmail.com>
2023-10-17 15:15:51 +0800

    chore: update `WEB_VIEW_VERSION`

--- src/constant.ts
+++ src/constant.ts
@@ -3,7 +3,7 @@ import type { StatInkPostBody, VsHistoryDetail } from "./types.ts";
 export const AGENT_NAME = "s3si.ts";
 export const S3SI_VERSION = "0.4.9";
 export const NSOAPP_VERSION = "2.7.0";
-export const WEB_VIEW_VERSION = "4.0.0-091d4283";
+export const WEB_VIEW_VERSION = "4.0.0-dae4328c";
 export enum Queries {
   HomeQuery =
     "51fc56bbf006caf37728914aa8bc0e2c86a80cf195b4d4027d6822a3623098a8",
