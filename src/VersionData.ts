6a4fd3ccebfd998569b2d267c48c56ab37d068a8
spacemeowx2 <spacemeowx2@gmail.com>
2023-11-30 13:44:06 +0800

    chore: update VersionData

--- src/VersionData.ts
+++ src/VersionData.ts
@@ -36,6 +36,12 @@ export const SEASONS: Season[] = [
     start: new Date("2023-09-01T00:00:00+00:00"),
     end: new Date("2023-12-01T00:00:00+00:00"),
   },
+  {
+    id: "season202312",
+    name: "Chill Season 2023",
+    start: new Date("2023-12-01T00:00:00+00:00"),
+    end: new Date("2024-03-01T00:00:00+00:00"),
+  },
 ];
 
 export const getSeason = (date: Date): Season | undefined => {
